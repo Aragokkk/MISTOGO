152e755175b0beb70ea29ecd0cee12b00978908b
serj <serj080890@gmail.com>
2025-09-24 21:36:02 +0300

    commit 15

--- web/src/pages/Admin/Admin.tsx
+++ web/src/pages/Admin/Admin.tsx
@@ -1,8 +1,12 @@
+import BackHomeButton from "../../components/BackHomeButton";
+import BackButton from "../../components/BackButton";
 function Admin() {
   return (
     <div style={{ padding: "2rem" }}>
       <h1>Admin</h1>
       <p>Сторінка Admin.</p>
+       <BackHomeButton />
+      <BackButton />
     </div>
   );
 }
--- web/src/pages/User/User.tsx
+++ web/src/pages/User/User.tsx
@@ -1,8 +1,12 @@
+import BackHomeButton from "../../components/BackHomeButton";
+import BackButton from "../../components/BackButton";
 function User() {
   return (
     <div style={{ padding: "2rem" }}>
       <h1>User</h1>
       <p>Сторінка User.</p>
+      <BackHomeButton />
+      <BackButton />
     </div>
   );
 }
